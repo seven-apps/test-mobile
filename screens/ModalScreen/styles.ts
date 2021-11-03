931e6486ed93db94ab917587ab4752060166af4b
Andre Murilo <lifercode@gmail.com>
2021-11-03 18:44:37 -0300

    feat: info screen

---
+++ screens/ModalScreen/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30
+  },
+  section: {
+    marginBottom: 20
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 5
+  },
+  listItem: {
+    fontSize: 16,
+    marginBottom: 5
+  },
+})
+
+export default styles
